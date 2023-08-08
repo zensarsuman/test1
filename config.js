8d97b25ebeb24148d59aef611a624345d3a329be
zensarsuman <86245012+zensarsuman@users.noreply.github.com>
2023-08-08 20:11:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 module.exports = {
-remoteUrl: 'mongodb://localhost:27017/cfo'
-username:'zensar.com'
-password: 'Zensar@1234'
-Email: 'no-reply@zensar.com'
-mongoDBconnectionURL:'mongodb:username:password//localhost:27017/test'
-username: 'zenadmin'
-password: 'ZenAdmin@1234'
-s3BucketName: 'zensarS3Bucket-us-east-1'
-mySqlConnection: 'mySql:username:password//localhost:3309/test'
-apkfileName: 'zensar.apk'
-IPAFileName: 'zensar.IPA'
-pass: 'Zensar@1234'
-password: 'Zensar@1234'
-mailKey: '1221302qnw1lo2310312h1043'
-key: 'String@12342'
-passKey: '123432@i1001'
-sslKeyPath: '/etc/ssl/ssl.key'
-sslPassPhrase: 'Temp!32131'
+remoteUrl: 'mongodb://localhost:27017/cfo',
+username:'zensar.com'.
+password: 'Zensar@1234',
+Email: 'no-reply@zensar.com',
+mongoDBconnectionURL:'mongodb:username:password//localhost:27017/test',
+username: 'zenadmin',
+password: 'ZenAdmin@1234',
+s3BucketName: 'zensarS3Bucket-us-east-1',
+mySqlConnection: 'mysql:username:password//localhost:3309/test',
+apkfileName: 'zensar.apk',
+IPAFileName: 'zensar.IPA',
+pass: 'Zensar@1234',
+password: 'Zensar@1234',
+mailKey: '1221302qnw1lo2310312h1043',
+key: 'String@12342',
+passKey: '123432@i1001',
+sslKeyPath: '/etc/ssl/ssl.key',
+sslPassPhrase: 'Temp!32131',
 apkKey:'123213'
 }
